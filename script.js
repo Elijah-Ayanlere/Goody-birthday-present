9369ff62f4786e3c8f4f8ae7f269268e88f10cf4
Elijah-Ayanlere <elijahayanlere3@gmail.com>
2024-05-07 14:08:27 +0100

    Emem

---
+++ script.js
@@ -0,0 +1,13 @@
+const greetingText = "THE DAY!";
+const speed = 100; // Typing speed in milliseconds
+let index = 0;
+
+function typeWriter() {
+    if (index < greetingText.length) {
+        document.getElementById("greeting").innerHTML += greetingText.charAt(index);
+        index++;
+        setTimeout(typeWriter, speed);
+    }
+}
+
+typeWriter(); // Start typing when the page loads
